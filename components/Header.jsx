cbc8e50b388081b903e51ca9e7804732365d6c51
Ansh Srivastava <112191696+AnshCinematic@users.noreply.github.com>
2025-11-05 16:32:18 +0530

    Update resume link to open in a new tab

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,8 @@ export default function Header() {
         </p>
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
             <a href="#contact" className='px-10 py-3 bg-black text-white rounded-full flex items-center gap-2  duration-300'>Contact me <Image src={assets.right_arrow_white} alt='' className='w-4'/> </a>
-            <a href="/newresume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 ' >My Resume <Image src={assets.download_icon} alt='' className='w-4'/> </a>
+           <a href="/newresume.pdf" target="_blank" rel="noopener noreferrer" className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2">My Resume<Image src={assets.download_icon} alt="Download icon" className="w-4" /></a>
+
         </div>
     </div>
   )
